439869d7095baabd2ed5d8f11aa484a488b20a3d
Filip Sufitchi <fsufitchi@gmail.com>
2016-04-04 17:32:25 -0400

    dockerfile to deploy relay server

--- relay/relayserver/serverlaunch.go
+++ relay/relayserver/serverlaunch.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-
-	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -17,30 +13,15 @@ type Config struct {
 	Host string
 }
 
-func parseConfiguration(confpath string) (c Config, err error) {
-	data, err := ioutil.ReadFile(confpath)
-	if err != nil {
-		return
-	}
-	err = json.Unmarshal(data, &c)
-	return
-}
-
 func main() {
-	confpath := flag.String("conf", "/etc/filebounce.json", "path to JSON config file")
-	flag.Parse()
-
-	conf, err := parseConfiguration(*confpath)
-	if err != nil {
-		panic(err)
-	}
+	conf := Config{":8080"}
 
 	relay.RegisterDownloadHandlers()
 	relay.RegisterUploadHandlers()
 	relay.RegisterStatusHandlers()
 
 	fmt.Fprintf(os.Stderr, "Serving on %s\n", conf.Host)
-	err = http.ListenAndServe(conf.Host, nil)
+	err := http.ListenAndServe(conf.Host, nil)
 	if err != nil {
 		panic(err)
 	}
